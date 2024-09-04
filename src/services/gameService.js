6ed8089f79b1dd3f1690c656465ece5adff8af56
ValeriCodeU <valeri.yanev@icloud.com>
2024-09-04 13:59:30 +0300

    Add get all games service method

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,22 +1,35 @@
-const baseUrl  = 'http:localhost:3030/jsonstore'
+import { request } from "../lib/request";
+
+const baseUrl  = 'http://localhost:3030/jsonstore/games'
+
+export const getAll = async () => {
+    let result = await request(baseUrl, 'GET');
+
+    result = Object.values(result);    
+    return result;
+}
 
 export const create = async (gameData) => {
 
     try {
-        const response = await fetch(`${baseUrl}/games`, {
+        const response = await fetch(baseUrl, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
             body: JSON.stringify(gameData)
         });
+
+          // Проверка за успешен отговор
+          if (!response.ok) {
+            throw new Error('Failed to create game');
+        }
     
-        const result = await response.json ();
+        const result = await response.json();
     
         return result;
         
     } catch (error) {
         console.log(error);
-    }
-    
+    }    
 }
