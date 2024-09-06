21ad5daa449caff5d093d2f24e3725dbb8a5f9d1
ValeriCodeU <valeri.yanev@icloud.com>
2024-09-06 14:36:29 +0300

    Modify request functionality

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,35 +1,27 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
-const baseUrl  = 'http://localhost:3030/jsonstore/games'
+const baseUrl = 'http://localhost:3030/jsonstore/games'
 
 export const getAll = async () => {
-    let result = await request(baseUrl, 'GET');
+    let result = await request.get(baseUrl);
 
-    result = Object.values(result);    
+    result = Object.values(result);
     return result;
 }
 
 export const create = async (gameData) => {
 
     try {
-        const response = await fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(gameData)
-        });
+        const result = await request.post(baseUrl, gameData);
 
-          // Проверка за успешен отговор
-          if (!response.ok) {
+        // Проверка за успешен отговор
+        if (!result.ok) {
             throw new Error('Failed to create game');
         }
-    
-        const result = await response.json();
-    
+
         return result;
-        
+
     } catch (error) {
         console.log(error);
-    }    
+    }
 }
