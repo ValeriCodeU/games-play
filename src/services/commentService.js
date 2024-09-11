676027bbfc7f515e9270b769247214d375993acd
ValeriCodeU <valeri.yanev@icloud.com>
2024-09-11 10:33:10 +0300

    Create comment service

---
+++ src/services/commentService.js
@@ -0,0 +1,13 @@
+import * as request from '../lib/request';
+
+const baseUrl = 'http://localhost:3030/jsonstore/comments';
+
+export const create =  async (gameId, userName, text) => {
+    const result = await request.post(baseUrl, {
+        gameId,
+        userName,
+        text
+    });
+
+    return result;
+}
