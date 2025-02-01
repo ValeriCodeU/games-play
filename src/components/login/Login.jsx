ef5b458dfa32cb1669e2105e4692fc75c05adcae
ValeriCodeU <valeri.yanev@icloud.com>
2025-02-02 00:58:17 +0200

    Add login form keys

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,22 @@
 import useForm from "../../hooks/useForm";
 
-export default function Login() {
+const LogintFormKeys = {
+    Email: 'email',
+    Password: 'password'
+}
 
-    const {values, onChange, onSubmit} = useForm({
-        email: '',
-        password: '',
+export default function Login({
+    loginSubmitHandler,
+}) {
+
+    const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+        [LogintFormKeys.Email]: '',
+        [LogintFormKeys.Password]: '',
     });
 
     return (
         <section id="login-page" className="auth">
-            <htmlForm id="login" onSubmit={onSubmit}>
+            <form id="login" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
@@ -17,26 +24,26 @@ export default function Login() {
                     <input
                         type="email"
                         id="email"
-                        name="email"
+                        name={LogintFormKeys.Email}
                         placeholder="Sokka@gmail.com"
                         onChange={onChange}
-                        value={values.email}
+                        value={values[LogintFormKeys.Email]}
                     />
 
                     <label htmlFor="login-pass">Password:</label>
-                    <input 
-                    type="password" 
-                    id="login-password" 
-                    name="password"
-                    onChange={onChange}
-                    value={values.password} 
+                    <input
+                        type="password"
+                        id="login-password"
+                        name={LogintFormKeys.Password}
+                        onChange={onChange}
+                        value={values[LogintFormKeys.Password]}
                     />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don&apos;t have profile click <a href="#">here</a></span>
                     </p>
                 </div>
-            </htmlForm>
+            </form>
         </section>
     );
 }
