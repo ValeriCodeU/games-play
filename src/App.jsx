afd26e3022f7830497d0c35d4d90d0ce3d7e850a
ValeriCodeU <valeri.yanev@icloud.com>
2025-03-02 00:18:37 +0200

    Add login redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
+import { useState } from "react"
+import { Routes, Route, useNavigate } from "react-router-dom"
+
+import AuthContext from "./contexts/AuthContext"
+import * as autService from './services/authService'
+import Path from "../paths"
+
 import GameCreate from "./components/game-create/GameCreate"
 import GameList from "./components/game-list/GameList"
 import Header from "./components/header/Header"
 import HomePage from "./components/home/HomePage"
-import { Routes, Route } from "react-router-dom"
 import Login from "./components/login/Login"
 import Register from "./components/register/Register"
 import GameDetails from "./components/game-details/GameDetails"
-import { useState } from "react"
-import AuthContext from "./contexts/authContext"
 
 function App() {
 
   //To be moved to a more appropriate place
   const [auth, setAuth] = useState({});
+  const navigate = useNavigate();
 
-  const loginSubmitHandler = (values) => {
+  const loginSubmitHandler = async values => {
     console.log(values);
+
+    const response = await autService.login(values.email, values.password);
+    
+    console.log(response);
+    setAuth(response);
+
+    navigate(Path.Home);
+
+    
   }
 
   return (
 
-    <AuthContext.Provider value={{loginSubmitHandler}}>
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
       <>
         <Header />
         <Routes >
           <Route path="/" element={<HomePage />} />
           <Route path="/games" element={<GameList />} />
           <Route path="/create" element={<GameCreate />} />
-          <Route path="/login" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/games/:gameId" element={<GameDetails />} />
         </Routes>
