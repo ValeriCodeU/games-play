43dfe44dd92ce6625ed21a47477fd9b56d570220
ValeriCodeU <valeri.yanev@icloud.com>
2025-03-28 17:29:19 +0200

    Change practise server service

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -1,6 +1,6 @@
 import * as request from '../lib/request';
 
-const baseUrl = 'http://localhost:3030/jsonstore/comments';
+const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (gameId) => {
     const result = await request.get(baseUrl);
