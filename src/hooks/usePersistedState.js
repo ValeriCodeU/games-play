31a520617ba99d79568d29b31daf8669c771506c
ValeriCodeU <valeri.yanev@icloud.com>
2025-04-11 18:00:05 +0300

    Implement custom hook for persisted state

--- src/hooks/usePersistedState.js
+++ src/hooks/usePersistedState.js
@@ -1,11 +1,27 @@
 import { useState } from "react";
 
-export default function usePersistedState(defaultValue) {
-    const [state, setState] = useState(defaultValue);
+export default function usePersistedState(key, defaultValue) {
+    const [state, setState] = useState(() => {
 
-    const setPersistedState = () => {
+        const persistedState = localStorage.getItem(key);
 
-    }
+        if (persistedState) {
+            return JSON.parse(persistedState);
+        }
+
+        return defaultValue;
+
+    });
+
+    const setPersistedState = (value) => {
+
+        setState(value);
+
+        const serializedValue = JSON.stringify(value);
+
+        localStorage.setItem(key, serializedValue);
+
+    };
 
     return [
         state,
